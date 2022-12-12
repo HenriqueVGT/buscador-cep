49387aa004db8cf76c8a3e74e3d67f4ca5143805
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-11 21:26:43 -0300

    chore: Code review

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
+//  Mantenha o padrão de aspas e estilização de formatação do código
+//  Para facilitar e automatizar isso você pode utilizar o eslint
 import axios from 'axios';
 
 const api = axios.create ({
     baseURL: "https://viacep.com.br/ws/"
 }); 
 
+// Tente sempre utilizar export ao invés do export default
 export default api;
